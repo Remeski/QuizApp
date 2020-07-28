a0f5e385d9a845b8f44ea52e11da63e1abdf2350
Remesk1 <green.block909@gmail.com>
2020-07-28 22:33:34 +0300

    Added PieChart to other user answers

--- lib/AnswersPage.dart
+++ lib/AnswersPage.dart
@@ -3,6 +3,7 @@ import 'package:CodeQuiz/Question.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class AnswersPage extends StatefulWidget {
   final List<UserAnswer> userAnswers;
@@ -46,61 +47,86 @@ class _AnswerPageState extends State<AnswersPage> {
                         itemCount: userAnswers.length,
                         itemBuilder: (context, i) {
                           var otherUserAnswers = data.userAnswers;
-                          var percentageList;
-                          for (var k = 0; k < otherUserAnswers.length; k++) {
+                          //var percentageList;
+                          /*for (var k = 0; k < otherUserAnswers.length; k++) {
                             if (otherUserAnswers[k]["question"] ==
                                 userAnswers[i].question.name) {
                               percentageList = PercentageOfAnswers(
                                       otherUserAnswers[k]["answers"])
                                   .returnlist;
                             }
-                          }
-                          return Card(
-                              color: Colors.white,
-                              child: Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                        "Question: ${userAnswers[i].question.name}"),
-                                    Text("You answered:"),
-                                    ListView.builder(
-                                      shrinkWrap: true,
-                                      itemCount:
-                                          userAnswers[i].answerslist.length,
-                                      itemBuilder: (context, j) {
-                                        return Text(
-                                            "- " +
-                                                userAnswers[i]
-                                                    .answerslist[j]
-                                                    .name,
-                                            textAlign: TextAlign.center);
-                                      },
-                                    ),
-                                    Text("What other people answered: "),
-                                    ListView.builder(
-                                      shrinkWrap: true,
-                                      itemCount: percentageList.length,
-                                      itemBuilder: (context, j) {
-                                        return Row(
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                          children: [
-                                          Text("- " +
-                                              percentageList[j]["answer"] + ": "),
-                                          Text(percentageList[j]["percentage"], style: TextStyle(fontWeight: FontWeight.bold))
-                                        ]);
-                                      },
-                                    ),
-                                  ],
-                                ),
-                              ));
+                          }*/
+                          return GestureDetector(
+                            onTap: () {
+                              for (var k = 0;
+                                  k < otherUserAnswers.length;
+                                  k++) {
+                                if (otherUserAnswers[k]["question"] ==
+                                    userAnswers[i].question.name) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ShowPercentage(
+                                              otherUserAnswers[k])));
+                                }
+                              }
+                            },
+                            child: Card(
+                                color: Colors.white,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                          "Question: ${userAnswers[i].question.name}"),
+                                      Text("You answered:"),
+                                      ListView.builder(
+                                        shrinkWrap: true,
+                                        itemCount:
+                                            userAnswers[i].answerslist.length,
+                                        itemBuilder: (context, j) {
+                                          return Text(
+                                              "- " +
+                                                  userAnswers[i]
+                                                      .answerslist[j]
+                                                      .name,
+                                              textAlign: TextAlign.center);
+                                        },
+                                      ),
+                                      /*Text("What other people answered: "),
+                                      ListView.builder(
+                                        shrinkWrap: true,
+                                        itemCount: percentageList.length,
+                                        itemBuilder: (context, j) {
+                                          return Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Text("- " +
+                                                    percentageList[j]["answer"] +
+                                                    ": "),
+                                                Text(
+                                                    percentageList[j]
+                                                        ["percentage"],
+                                                    style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold))
+                                              ]);
+                                        },
+                                      ),*/
+                                    ],
+                                  ),
+                                )),
+                          );
                         });
                   }
                   if (snapshot.hasError) {
                     return Text("Error ${snapshot.error}");
                   }
-                  return Center(child: Column(
+                  return Center(
+                      child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       CircularProgressIndicator(),
@@ -111,6 +137,48 @@ class _AnswerPageState extends State<AnswersPage> {
                 });
           }),
         ));
+  }
+}
+
+class ShowPercentage extends StatelessWidget {
+  var json;
+  Map<String, double> dataMap = Map();
+  ShowPercentage(this.json) {
+    for (var i = 0; i < json["answers"].length; i++) {
+      dataMap.putIfAbsent(json["answers"][i]["answer"],
+          () => double.parse(json["answers"][i]["useranswers"]));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(json["question"]);
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        title: Text(
+          json["question"],
+          style: TextStyle(color: Colors.white),
+        ),
+        //centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Center(
+          child: PieChart(dataMap: dataMap),
+        ),
+      ),
+    );
   }
 }
 
